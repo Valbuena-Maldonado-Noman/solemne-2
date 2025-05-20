defa98595814e973ad084445b5be1a62c353a71c
saavz <savalbuenaz@gmail.com>
2025-05-20 15:03:17 -0400

    feature:grid añadiendo los componentes estilo y grid

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { GridComponent } from './grid/grid.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: '', component: GridComponent },
+     ];
+
+
